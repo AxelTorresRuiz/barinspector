c100db36e097f777e896b3cda2a2a064bf6457f3
AxelTorresRuiz <19cg0121@itsncg.edu.mx>
2023-09-11 13:54:21 -0600

    feat: Add visual option to add a new div with a button

--- src/app/components/pages/configuration/configuration.component.ts
+++ src/app/components/pages/configuration/configuration.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PagetitleService } from 'src/app/services/pagetitle.service';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { NewbarModalComponent } from '../../modals/newbar-modal/newbar-modal.component';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-configuration',
   templateUrl: './configuration.component.html',
@@ -14,19 +15,30 @@ export class ConfigurationComponent implements OnInit {
 
   inputDisabled:boolean = false;
 
+  divs: Array<{ barName: string, checked: boolean }> = [];
+
   toggleInput(){
     this.inputDisabled= !this.inputDisabled;
   }
 
-  openModal(){
-    const dialogRef = this.dialog.open(NewbarModalComponent,{
-      width:'52.875rem', height:'42.75rem',
-      disableClose:true,
-
+  openModal() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '52.875rem';
+    dialogConfig.height = '42.75rem';
+    dialogConfig.disableClose = true;
+    dialogConfig.data = {
+      divs: this.divs // Pasar la lista divs al modal
+    };
+    
+    const dialogRef = this.dialog.open(NewbarModalComponent, dialogConfig);
+  
+    dialogRef.afterClosed().subscribe((result: Array<{ barName: string, checked: boolean }>) => {
+      if (result) {
+        this.divs = result; // Actualiza la lista divs con los datos del modal
+      }
     });
-
   }
-
+  
   moveadd(){
     this.router.navigate(['/addbottle'])
   }
