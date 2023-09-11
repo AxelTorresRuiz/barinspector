ae324a49520c53d96142f0d31ef7383ed2431a3a
AxelTorresRuiz <19cg0121@itsncg.edu.mx>
2023-09-11 15:26:55 -0600

    fix: adapt the modal to the changes made for the bar divs

--- src/app/components/modals/newbar-modal/newbar-modal.component.ts
+++ src/app/components/modals/newbar-modal/newbar-modal.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Inject } from '@angular/core';
 @Component({
   selector: 'app-newbar-modal',
   templateUrl: './newbar-modal.component.html',
   styleUrls: ['./newbar-modal.component.css']
 })
 export class NewbarModalComponent {
-  constructor(private dialogRef: MatDialogRef<NewbarModalComponent>){}
+  constructor(private dialogRef: MatDialogRef<NewbarModalComponent>, @Inject(MAT_DIALOG_DATA) public data: any ){}
+  
+
+  newBarName: string = ''; 
+  newChecked: boolean = false;
 
   closeModal(){
     this.dialogRef.close()
+  }
+
+  onSubmit() {
+    const newDiv = { barName: this.newBarName, checked: this.newChecked };
+    this.data.divs.push(newDiv);
+    this.dialogRef.close(this.data.divs);
   }
 
 
