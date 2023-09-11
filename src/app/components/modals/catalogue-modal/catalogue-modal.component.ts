bd7e744524bb6adeac0381a39af1814b840bc407
AxelTorresRuiz <19cg0121@itsncg.edu.mx>
2023-09-11 15:27:42 -0600

    fix: adapt bottle modal based on api changes

--- src/app/components/modals/catalogue-modal/catalogue-modal.component.ts
+++ src/app/components/modals/catalogue-modal/catalogue-modal.component.ts
@@ -1,5 +1,5 @@
-import { Component,OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component,Inject,OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { BasculaService } from 'src/app/services/bascula.service';
 @Component({
   selector: 'app-catalogue-modal',
@@ -8,10 +8,12 @@ import { BasculaService } from 'src/app/services/bascula.service';
 })
 export class CatalogueModalComponent implements OnInit {
    heightValue!: number;
-  constructor(private bascula:BasculaService,private dialogRef: MatDialogRef<CatalogueModalComponent>){}
+  constructor(private bascula:BasculaService,private dialogRef: MatDialogRef<CatalogueModalComponent>, @Inject(MAT_DIALOG_DATA) public data:{botellaSeleccionada: any}){}
 
   button1Active: boolean = false;
   button2Active: boolean = false;
+  btn1active: boolean=false;
+  btn2active: boolean=false;
 
   toggleButton(button: string): void {
     if (button === 'button1') {
@@ -21,9 +23,15 @@ export class CatalogueModalComponent implements OnInit {
     }
   }
 
+  tglButton(button: string): void {
+    if (button === 'btn1') {
+      this.btn1active = !this.btn1active;
+    } else if (button === 'btn2') {
+      this.btn2active = !this.btn2active;
+    }
+  }
   ngOnInit(){
     this.bascula.startConnection();
-
     this.bascula.getWeightObservable().subscribe(weight=>{
       this.heightValue = weight;
     })
