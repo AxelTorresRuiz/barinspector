02f8ddf100deb25c0d34b2efdfb964d126e58c17
AxelTorresRuiz <19cg0121@itsncg.edu.mx>
2023-09-27 12:49:02 -0600

    feat: adapt the page to make the view responsive
    deprecated services were deleted in the ts file

--- src/app/components/pages/mybar/mybar.component.ts
+++ src/app/components/pages/mybar/mybar.component.ts
@@ -1,18 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PagetitleService } from 'src/app/services/pagetitle.service';
-import { BottleserviceService } from 'src/app/services/api/bottleservice.service';
 import { BasculaService } from 'src/app/services/bascula.service';
 import { bottleGet, bottlePost, bottlePut } from 'src/app/services/api/Bottle.service'
 import { Bottle } from 'src/app/services/api/Bottle.model';
 import { BarBottle } from 'src/app/services/api/BarBottle.model';
+import { ChartType, ChartOptions, ChartDataset } from 'chart.js';
+
 @Component({
   selector: 'app-mybar',
   templateUrl: './mybar.component.html',
   styleUrls: ['./mybar.component.css']
 })
+
+
+
+
 export class MybarComponent implements OnInit {
-  constructor(private pageTitleService:PagetitleService, private router:Router, private bottleService:BottleserviceService, private bascula: BasculaService ){
+
+  public pieChartOptions: ChartOptions ={
+    responsive: true,
+  };
+  public pieChartData: ChartDataset[]=[
+    {
+      data:[30,4]
+    },
+  ];
+  public pieChartType: ChartType = 'pie';
+
+
+
+
+  constructor(private pageTitleService:PagetitleService, private router:Router, private bascula: BasculaService ){
   }
 
   agregar(){
