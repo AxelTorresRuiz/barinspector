480da026e85c6ebeee9c6010b8998da37914404d
AxelTorresRuiz <19cg0121@itsncg.edu.mx>
2023-09-27 12:37:48 -0600

    feat: adapt module dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {RouterModule, Routes} from '@angular/router'
 import * as signalR from '@microsoft/signalr';
+import { NgChartsModule } from 'ng2-charts';
 import {MatDialogModule} from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -18,14 +19,13 @@ import { CatalogueComponent } from './components/pages/catalogue/catalogue.compo
 import { APP_ROUTING } from './app.routes';
 import { ConfigurationComponent } from './components/pages/configuration/configuration.component';
 import { PagetitleService } from './services/pagetitle.service';
-import { BottleserviceService } from './services/api/bottleservice.service';
-import { CatalogueService } from './services/api/catalogue.service';
 import { BasculaService } from './services/bascula.service';
 import { CatalogueModalComponent } from './components/modals/catalogue-modal/catalogue-modal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NewbarModalComponent } from './components/modals/newbar-modal/newbar-modal.component';
 import { AddbottleComponent } from './components/pages/addbottle/addbottle.component';
 import { AddmanagerComponent } from './components/pages/addmanager/addmanager.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
@@ -42,7 +42,8 @@ import { AddmanagerComponent } from './components/pages/addmanager/addmanager.co
     CatalogueModalComponent,
     NewbarModalComponent,
     AddbottleComponent,
-    AddmanagerComponent
+    AddmanagerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -51,11 +52,11 @@ import { AddmanagerComponent } from './components/pages/addmanager/addmanager.co
     BrowserAnimationsModule,
     MatDialogModule,
     FormsModule,
+    NgChartsModule
+
   ],
   providers: [
     PagetitleService,
-    BottleserviceService,
-    CatalogueService,
     BasculaService
     
   ],
