81312a6387fec15d76c90fc9ab265768501b4557
Axel Torres Ruiz <19cg0121@itsncg.edu.mx>
2023-10-27 15:51:13 -0600

    feat: create audit modal scan

--- src/app/components/modals/audit-modal/audit-modal.component.ts
+++ src/app/components/modals/audit-modal/audit-modal.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { AuditscanModalComponent } from '../auditscan-modal/auditscan-modal.component';
 @Component({
   selector: 'app-audit-modal',
   templateUrl: './audit-modal.component.html',
   styleUrls: ['./audit-modal.component.css']
 })
 export class AuditModalComponent {
+  constructor(private dialog:MatDialog, public dialogRef:MatDialogRef<AuditscanModalComponent>){}
+
+  openDialog(){
+    this.dialog.open(AuditscanModalComponent,{
+      width:'41.875', height:'27.25rem'
+    })
+    this.dialogRef.close();
+  }
+
+  closeModal(){
+    this.dialogRef.close()
+  }
 
 }
